f84f82dfa8abea06736c26a9af052da00ab893f4
kchen328 <43022233+kchen328@users.noreply.github.com>
2021-03-07 21:03:07 -0500

    Add files via upload

---
+++ maximumSubarray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // input: array nums
+        // output: largest sum
+        // Kadane's algorithm
+        
+        // initialize sum to 0
+        int currMax = nums[0], globalMax = currMax;
+        for(int i = 1; i < nums.length; i++) {
+            // compare current max to previous max + current max
+            currMax = Math.max(nums[i], currMax += nums[i]);
+            // keep track of greatest current max
+            if (currMax > globalMax) {
+                globalMax = currMax;
+            }
+        }
+        return globalMax;
+        
+    }
+}
