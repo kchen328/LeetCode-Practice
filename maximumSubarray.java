155acf4b78441f0c74c9b7cd87922d380cbe9cd0
kchen328 <43022233+kchen328@users.noreply.github.com>
2021-03-07 22:53:26 -0500

    Update maximumSubarray.java

--- maximumSubarray.java
+++ maximumSubarray.java
@@ -7,7 +7,7 @@ class Solution {
         // initialize sum to 0
         int currMax = nums[0], globalMax = currMax;
         for(int i = 1; i < nums.length; i++) {
-            // compare current max to previous max + current max
+            // compare current to previous + current
             currMax = Math.max(nums[i], currMax += nums[i]);
             // keep track of greatest current max
             if (currMax > globalMax) {
