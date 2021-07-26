50283545e347a7d42d706f26f4d8635754ed8c4d
kchen328 <43022233+kchen328@users.noreply.github.com>
2021-07-25 21:15:49 -0400

    Create houseRobberII.java

---
+++ houseRobberII.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        return Math.max(robHelper(Arrays.copyOfRange(nums, 0, nums.length - 1)), robHelper(Arrays.copyOfRange(nums, 1, nums.length)));
+    }
+    
+    public int robHelper(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int[] dp = new int[nums.length];
+        // first house
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.max(nums[i] + dp[i-2], dp[i - 1]);
+
+        }
+            
+        return dp[nums.length - 1];
+    }
+}
